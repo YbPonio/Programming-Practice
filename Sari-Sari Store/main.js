a48e0816e24a7be11429bcf069ea6705db047856
ycker <ybponio@gmail.com>
2024-02-05 07:50:06 -0800

    commit

--- Sari-Sari Store/main.js
+++ Sari-Sari Store/main.js
@@ -3,29 +3,24 @@ let products = [
     {
         image: './img/item-1.avif',
         title: 'Cerruti 1881 Mucciano Watch',
-        price: '$700',
+        price: 400,
         stock: 20,
     },
     {
         image: './img/item-2.avif',
         title: 'New Port',
-        price: '$900',
+        price: 900,
         stock: 63,
     },
     {
         image: './img/item-3.avif',
         title: 'Nmierer',
-        price: '$400',
-        stock: 40,
+        price: 700,
+        stock: 2,
     },
 ]
 
-let myPurchaseArray = [
-    {
-        title: 'sadsadsad',
-        quantity: 90,
-    }
-]
+let myPurchaseArray = [];
 
 function displayProducts() {
     shopContainer.innerHTML = '';
@@ -52,11 +47,11 @@ function displayProductsModal(value) {
                 <form method="dialog" onsubmit="buyingProducts(${value})">
                     <h1>${products[value].title}</h1>
                     <h2>${products[value].price}</h2>
-                    <h2>Stock:    <input id="productValue" type="number" value="${products[value].stock}"></h2>
-                    <h2>Quantity:    <input id="productQuantity" type="number" value="1"></h2>
+                    <h2>Stock: ${products[value].stock}</h2>
+                    <h2>Quantity:    <input id="productQuantity" type="number" value="1" min="1"></h2>
                     <div class="button-modal">
                         <button type="submit">Buy</button>
-                        <button>Cancel</button>
+                        <button type="button" onclick="buyingProductModal.close()">Cancel</button>
                     </div>
                 </form>
             </div>
@@ -66,9 +61,32 @@ function displayProductsModal(value) {
 }
 
 function buyingProducts(value) {
-    let finalProduct = products[value].stock - productQuantity.value;
-    products[value].stock = finalProduct;
+    if (products[value].stock >= productQuantity.value) {
+        let finalProduct = products[value].stock - productQuantity.value;
+        products[value].stock = finalProduct;
+    
+        let existingPurchaseItem = myPurchaseArray.find(item => item.title == products[value].title);
 
+        if (existingPurchaseItem) {
+            existingPurchaseItem.quantity += parseInt(productQuantity.value);
+        } else {
+            myPurchaseArray.push({
+                image: products[value].image,
+                title: products[value].title,
+                quantity: parseInt(productQuantity.value)
+            });
+        }
+
+        displayMyPurchase();
+
+        if (products[value].stock <= 0) {
+            products.splice(value, 1);
+        }
+    } else {
+        alert("Not enough stock available");
+    }
+
+    console.log(myPurchaseArray);
     displayProducts();
 }
 
@@ -91,15 +109,19 @@ function addingProducts() {
 }
 
 function displayMyPurchase() {
-    purchaseItemContainer.innerHTML = `
+    purchaseItemContainer.innerHTML = '';
+    for (let i = 0; i < myPurchaseArray.length; i++) {
+        let myItem = myPurchaseArray[i];
+        purchaseItemContainer.innerHTML += `
         <div class="item">
-        <img src="./img/item-2.avif" alt="">
-            <div>
-                <h4>fsdbfbdsjbjhfjhbTITLE</h4>
-                <p>Quantity: 20</p>
-            </div>
+        <img src="${myItem.image}" alt="${myItem.image}">
+        <div>
+            <h4>${myItem.title}</h4>
+            <p>Quantity: ${myItem.quantity}</p>
         </div>
-    `;
+    </div>
+        `;
+    }
 }
-
+displayMyPurchase();
 displayProducts();
