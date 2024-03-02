0df503bb175803603cc56514eb04bb8be8e10a69
ycker <ybponio@gmail.com>
2024-03-02 19:21:13 +0800

    commit

--- DIshCovery/main.js
+++ DIshCovery/main.js
@@ -1,11 +1,12 @@
 async function getData() {
-    let response = await fetch("https://ybponio.github.io/Programming-Practice/DIshCovery/storage.json");
-    let data = await response.json();
-    console.log(data);
+  let response = await fetch(
+    "https://ybponio.github.io/Programming-Practice/DIshCovery/storage.json"
+  );
+  let data = await response.json();
 }
 
 function toAnotherPage() {
-    window.location.href = 'recipe-page.html';
+  window.location.href = "recipe-page.html";
 }
 
-getData()
+getData();
