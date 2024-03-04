2db7c51bb77e2810b073c6e7b85c4626d21c0892
ycker <you@example.com>
2024-03-04 16:03:36 +0800

    commit

--- DIshCovery/main.js
+++ DIshCovery/main.js
@@ -1,9 +1,39 @@
+let recipes;
 async function getData() {
   let response = await fetch(
     "https://ybponio.github.io/Programming-Practice/DIshCovery/storage.json"
   );
   let data = await response.json();
-  console.log(data);
+  recipes = data;
+  console.log(recipes);
+}
+
+function renderFoods() {
+  /*html */
+  for (recipe of recipes) {
+    recipeItems.innerHTML = `
+      <div class="item">
+      <div class="img-holder">
+        <img
+          src="./img/emerson-vieira-cpkPJ-U9eUM-unsplash.jpg"
+          alt=""
+        />
+      </div>
+      <h1>beef ground pork</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Voluptatum corporis dolore ab dicta consequatur necessitatibus
+        aperiam, ullam quaerat deserunt veritatis.
+      </p>
+    
+      <div class="showMoreBtn">
+        <button class="cta" onclick="closeBtn()">
+          <span class="hover-underline-animation">Get Recipe</span>
+        </button>
+      </div>
+    </div>
+      `;
+  }
 }
 
 function toAnotherPage() {
--- Sari-Sari Store/main.js
+++ Sari-Sari Store/main.js
@@ -1,32 +1,31 @@
-
 let products = [
-    {
-        image: './img/item-1.avif',
-        title: 'Cerruti 1881 Mucciano Watch',
-        price: 400,
-        stock: 20,
-    },
-    {
-        image: './img/item-2.avif',
-        title: 'New Port',
-        price: 900,
-        stock: 63,
-    },
-    {
-        image: './img/item-3.avif',
-        title: 'Nmierer',
-        price: 700,
-        stock: 2,
-    },
-]
+  {
+    image: "./img/item-1.avif",
+    title: "Cerruti 1881 Mucciano Watch",
+    price: 400,
+    stock: 20,
+  },
+  {
+    image: "./img/item-2.avif",
+    title: "New Port",
+    price: 900,
+    stock: 63,
+  },
+  {
+    image: "./img/item-3.avif",
+    title: "Nmierer",
+    price: 700,
+    stock: 2,
+  },
+];
 
 let myPurchaseArray = [];
 
 function displayProducts() {
-    shopContainer.innerHTML = '';
-    for (let i = 0; i < products.length; i++) {
-        let product = products[i];
-        shopContainer.innerHTML += `
+  shopContainer.innerHTML = "";
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i];
+    shopContainer.innerHTML += `
         <div class="item" onclick="displayProductsModal(${i})">
         <img src="${product.image}" alt="${product.image}">
         <div class="product-description">
@@ -35,12 +34,11 @@ function displayProducts() {
         </div>
         </div>
         `;
-    }
-   
+  }
 }
 
 function displayProductsModal(value) {
-    buyingProductModal.innerHTML = `
+  buyingProductModal.innerHTML = `
     <div class="container">
                 <div class="image-holder">
                     <img src="${products[value].image}" alt="">
@@ -57,63 +55,63 @@ function displayProductsModal(value) {
                 </form>
             </div>
     `;
-    buyingProductModal.showModal();
-    displayProducts();
+  buyingProductModal.showModal();
+  displayProducts();
 }
 
 function buyingProducts(value) {
-    if (products[value].stock >= productQuantity.value) {
-        let finalProduct = products[value].stock - productQuantity.value;
-        products[value].stock = finalProduct;
-    
-        let existingPurchaseItem = myPurchaseArray.find(item => item.title == products[value].title);
+  if (products[value].stock >= productQuantity.value) {
+    let finalProduct = products[value].stock - productQuantity.value;
+    products[value].stock = finalProduct;
 
-        if (existingPurchaseItem) {
-            existingPurchaseItem.quantity += parseInt(productQuantity.value);
-        } else {
-            myPurchaseArray.push({
-                image: products[value].image,
-                title: products[value].title,
-                quantity: parseInt(productQuantity.value)
-            });
-        }
+    let existingPurchaseItem = myPurchaseArray.find(
+      (item) => item.title == products[value].title
+    );
 
-        displayMyPurchase();
-
-        if (products[value].stock <= 0) {
-            products.splice(value, 1);
-        }
+    if (existingPurchaseItem) {
+      existingPurchaseItem.quantity += parseInt(productQuantity.value);
     } else {
-        displayError();
+      myPurchaseArray.push({
+        image: products[value].image,
+        title: products[value].title,
+        quantity: parseInt(productQuantity.value),
+      });
     }
 
-    console.log(myPurchaseArray);
-    displayProducts();
+    displayMyPurchase();
+
+    if (products[value].stock <= 0) {
+      products.splice(value, 1);
+    }
+  } else {
+    displayError();
+  }
+
+  console.log(myPurchaseArray);
+  displayProducts();
 }
 
 function addingProducts() {
-    let newUrl = urlInput.value;
-    let newTitle = titleInput.value;
-    let newPrice = priceInput.value;
-    let newStocks = stocksInput.value;
+  let newUrl = urlInput.value;
+  let newTitle = titleInput.value;
+  let newPrice = priceInput.value;
+  let newStocks = stocksInput.value;
 
-    products.push(
-        {
-            image: newUrl,
-            title: newTitle,
-            price: newPrice,
-            stock: newStocks,
-        }
-    );
+  products.push({
+    image: newUrl,
+    title: newTitle,
+    price: newPrice,
+    stock: newStocks,
+  });
 
-    displayProducts();
+  displayProducts();
 }
 
 function displayMyPurchase() {
-    purchaseItemContainer.innerHTML = '';
-    for (let i = 0; i < myPurchaseArray.length; i++) {
-        let myItem = myPurchaseArray[i];
-        purchaseItemContainer.innerHTML += `
+  purchaseItemContainer.innerHTML = "";
+  for (let i = 0; i < myPurchaseArray.length; i++) {
+    let myItem = myPurchaseArray[i];
+    purchaseItemContainer.innerHTML += `
         <div class="item">
         <img src="${myItem.image}" alt="${myItem.image}">
         <div>
@@ -122,13 +120,12 @@ function displayMyPurchase() {
         </div>
     </div>
         `;
-    }
+  }
 }
 
-function displayError(){
-    errorModal.showModal();
+function displayError() {
+  errorModal.showModal();
 }
-
 
 displayMyPurchase();
 displayProducts();
