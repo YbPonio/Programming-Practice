4848035584c340371ccf9a10ee27f0e745ca9636
ycker <you@example.com>
2024-03-14 17:01:35 +0800

    php

--- Inventory System/api/api.php
+++ Inventory System/api/api.php
@@ -1,22 +1,18 @@
 <?php
-try{
-    $handler = new PDO('mysql:host=localhost;dbname=fabs', 'fabs' , 'PmGzcScNHI7_/lA@');
-    $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} catch(PDOException $e) {
+use FFI\Exception;
+
+try {
+    $handler = new PDO('mysql:host=localhost;dbname=fabs', 'fabs', 'PmGzcScNHI7_/lA@');
+    // $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
     // die("Soory, please try again!");
-    echo $e->getMessage();
+    echo "Naay Error!";
 }
 
-$data = $handler->query('SELECT * FROM inventory');
+// $data = $handler->query('SELECT * FROM inventory');
 
-while ($result = $data->fetch()) {
-    echo $result['name'], '<br>';
-}
-
-
-// $result = $data->fetch();
-// echo '<pre>', print_r($result), '</pre>';
-
-
-?>
+// while ($result = $data->fetch()) {
+//     echo $result['name'], '<br>';
+// }
